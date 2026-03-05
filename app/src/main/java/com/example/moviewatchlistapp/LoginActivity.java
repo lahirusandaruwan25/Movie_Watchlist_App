c384ca4e6a64613f8789ccc64b8cc5be384cc2f5
SintyGamage <wanasinghedammika1966@gmail.com>
2026-03-05 11:46:21 +0530

    Add login screen and validation logic
    - Created `LoginActivity` with email and password validation patterns.
    - Added `activity_login.xml` layout featuring a logo, input fields, and login button.
    - Updated `strings.xml` with necessary labels for the login and registration flow.

---
+++ app/src/main/java/com/example/moviewatchlistapp/LoginActivity.java
@@ -0,0 +1,71 @@
+package com.example.moviewatchlistapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText etEmail, etPassword;
+    Button btnLogin;
+    TextView tvRegister;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        tvRegister = findViewById(R.id.tvRegister);
+        btnLogin.setOnClickListener(v -> {
+
+            String email = etEmail.getText().toString().trim();
+            String password = etPassword.getText().toString().trim();
+
+            if (!isValidEmail(email)) {
+                etEmail.setError("Invalid Email");
+            }
+            else if (!isValidPassword(password)) {
+                etPassword.setError("Password must be 8 chars, 1 capital letter, 1 number");
+            }
+            else if (email.isEmpty() || password.isEmpty()) {
+                etEmail.setError("All fields required");
+                etPassword.setError("All fields required");
+            }
+            else {
+                // Validation OK → Go to MainActivity
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
+                Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+
+    // Email Validation
+    public boolean isValidEmail(String email){
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    // Password Validation
+    public boolean isValidPassword(String password){
+        String passwordPattern = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
+        return password.matches(passwordPattern);
+    }
+}
